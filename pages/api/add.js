b8d1fce38a55ab2e69be7df1cca02cad3109618b
JoeHO888 <joe.hochotai@gmail.com>
2023-01-24 09:47:05 +0800

    Fix#1 - Use environment variables in APIs

--- pages/api/add.js
+++ pages/api/add.js
@@ -4,8 +4,8 @@ export default async (req, res) => {
     }
     let todo = encodeURI(req.query.todo)
 
-    const token = "REPLACE_YOUR_TOKEN";
-    const url = "https://REPLACE_YOUR_ENDPOINT/lpush/todo/" + todo + "?_token=" + token;
+    const token = process.env.UPSTASH_TOKEN;
+    const url = `https://${process.env.UPSTASH_DOMAIN}/lpush/todo/` + todo + "?_token=" + token;
 
     return fetch(url)
         .then(r => r.json())
--- pages/api/list.js
+++ pages/api/list.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
-    const token = "REPLACE_YOUR_TOKEN";
-    const url = "https://REPLACE_YOUR_ENDPOINT/lrange/todo/0/100?_token=" + token;
+    const token = process.env.UPSTASH_TOKEN;
+    const url = `https://${process.env.UPSTASH_DOMAIN}/lrange/todo/0/100?_token=` + token;
 
     return fetch(url)
         .then(r => r.json())
